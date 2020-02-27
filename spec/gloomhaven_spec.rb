ad8a41af6b7f64b6c15bf840c9fd03973e59a1fa
Teo Dell'Amico <teo.dellamico@gmail.com>
2020-02-26 19:54:46 -0800

    Added sunkeeper, renamed sun->light, night->dark'

--- spec/gloomhaven_spec.rb
+++ spec/gloomhaven_spec.rb
@@ -8,8 +8,33 @@ RSpec.describe Gloomhaven do
   end
 
   describe '::CARDS' do
-    it { expect(Gloomhaven::CARDS).not_to be nil }
-    it { expect(Gloomhaven::CARDS).to be_a(Array) }
-    it { expect(Gloomhaven::CARDS.length).to be > 0 }
+    subject(:cards) { Gloomhaven::CARDS }
+    it { expect(cards).not_to be nil }
+    it { expect(cards).to be_a(Array) }
+    it { expect(cards.length).to be > 0 }
+  end
+
+  describe '::CHARACTERS' do
+    subject(:characters) { Gloomhaven::CHARACTERS }
+    it { expect(characters).not_to be nil }
+    it { expect(characters).to be_a(Hash) }
+    it { expect(characters.length).to eq(17) }
+
+    it 'maps all character classes' do
+      expect(characters.keys).to include('brute')
+      expect(characters.keys).to include('cragheart')
+      expect(characters.keys).to include('mindthief')
+      expect(characters.keys).to include('tinkerer')
+      expect(characters.keys).to include('scoundrel')
+      expect(characters.keys).to include('spellweaver')
+      expect(characters.keys).to include('sunkeeper')
+    end
+  end
+
+  describe '::PERKS' do
+    subject(:perks) { Gloomhaven::PERKS }
+    it { expect(perks).not_to be nil }
+    it { expect(perks).to be_a(Hash) }
+    it { expect(perks.length).to be > 0 }
   end
 end
