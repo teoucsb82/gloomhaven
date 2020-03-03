00762ada78b4f3d24dbfda64d2852bde8273c2e9
Teo Dell'Amico <teo.dellamico@gmail.com>
2020-03-02 19:49:40 -0800

    Moved yaml back to config

--- lib/gloomhaven.rb
+++ lib/gloomhaven.rb
@@ -8,9 +8,9 @@ require 'gloomhaven/perk'
 module Gloomhaven
   class CardNotFoundError < StandardError; end
 
-  CARDS = YAML.load(File.read('lib/config/cards.yml'))
-  CHARACTERS = YAML.load(File.read('lib/config/characters.yml'))
-  PERKS = YAML.load(File.read('lib/config/perks.yml'))
+  CARDS = YAML.load(File.read('config/cards.yml'))
+  CHARACTERS = YAML.load(File.read('config/characters.yml'))
+  PERKS = YAML.load(File.read('config/perks.yml'))
 
   def self.version
     VERSION
